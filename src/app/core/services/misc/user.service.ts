cc1e8db8af3db4c45f86428901d3e545d21b017f
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-09-16 16:58:29 +0530

    minor fixes

--- src/app/core/services/misc/user.service.ts
+++ src/app/core/services/misc/user.service.ts
@@ -17,17 +17,6 @@ export class UserService {
     this.storageService.set('user', userProfile);
   }
 
-  storeFyleOrgsCount(): void {
-    const currentOrgsCount = this.storageService.get('orgsCount');
-    if (currentOrgsCount && currentOrgsCount > 1) {
-      // Do nothing - we already have the orgs count to show switch org button
-      return;
-    }
-    this.apiService.get(`/user/orgs/`, {}).subscribe((count: number) => {
-      this.storageService.set('orgsCount', count);
-    });
-  }
-
   getUserProfile(): MinimalUser {
     return this.storageService.get('user');
   }
