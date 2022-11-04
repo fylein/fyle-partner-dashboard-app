80456c0221e9fb4d93de4b518ab0e4fa8c05afd5
Anish Kr Singh <116036738+anishfyle@users.noreply.github.com>
2022-11-04 17:26:42 +0530

    auth-login End To End Test (#21)
    * added selectors
    
    * E2E test auth-login
    
    * made functions getElement/assertText

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -6,16 +6,19 @@ declare global {
   namespace Cypress {
     interface Chainable {
       login(): void;
+      getElement(attributeName: string): Cypress.Chainable<JQuery<HTMLElement>>;
+      assertText(attributeName: string, text: string): void;
     }
   }
 }
 
-function setupInterceptor(method: 'GET' | 'POST', url: string, alias: string) {
-  cy.intercept({
-    method: method,
-    url: `**${url}**`,
-  }).as(alias);
-}
+Cypress.Commands.add('assertText', (attributeName: string, text: string) => {
+  cy.getElement(attributeName).should('include.text', text)
+})
+
+Cypress.Commands.add('getElement', (attributeName: string) => {
+  return cy.get(`[data-cy=${attributeName}]`);
+})
 
 Cypress.Commands.add('login', () => {
   const user = {
@@ -27,8 +30,4 @@ Cypress.Commands.add('login', () => {
     org_id: environment.e2e_tests.org_id,
     org_name: 'XYZ Org'
   };
-  // window.localStorage.setItem('user', JSON.stringify(user))
-  // window.localStorage.setItem('workspaceId', environment.e2e_tests.workspace_id)
-
-  // cy.login() will be used in all tests, hence adding http listener here
 })
