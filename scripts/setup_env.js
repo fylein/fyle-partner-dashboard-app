11e7e91f54ace96def8d4c009b06403c3ed4035a
anish <anishkumar.s@fyle.in>
2022-11-04 11:30:16 +0530

    added fix

--- scripts/setup_env.js
+++ scripts/setup_env.js
@@ -6,6 +6,7 @@ const environment = {
   callback_uri: `${process.env.CALLBACK_URI ? process.env.CALLBACK_URI : '{{CALLBACK_URI}}'}`,
   api_url: `${process.env.API_URL ? process.env.API_URL : '{{API_URL}}'}`,
   fyle_app_url: `${process.env.FYLE_APP_URL ? process.env.FYLE_APP_URL : '{{FYLE_APP_URL}}'}`,
+  app_url: `${process.env.APP_URL ? process.env.APP_URL : '{{APP_URL}}'}`,
   sentry_dsn: `${process.env.SENTRY_DSN ? process.env.SENTRY_DSN : '{{SENTRY_DSN}}'}`,
   sentry_env: `${process.env.SENTRY_ENV ? process.env.SENTRY_ENV : '{{SENTRY_ENV}}'}`,
 };
