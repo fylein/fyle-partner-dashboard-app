0b65907b0243ec3588ed7dca5b0f0677ff9d1e1e
anish <anishkumar.s@fyle.in>
2022-11-08 15:28:48 +0530

    Refractoring Fixes #2

--- cypress/e2e/001-micro-actions/002-auth-logout.cy.js
+++ cypress/e2e/001-micro-actions/002-auth-logout.cy.js
@@ -6,13 +6,10 @@ describe('auth logout', () => {
     cy.visit('/')
   })
 
-  it('in partner-dashboard app : check logo/Home-text/username', () => {
+  it('in partner-dashboard app : check logo/Home-text/username then log out', () => {
     cy.getElement('fyle-logo')
     cy.assertText('header-home-text', 'Home')
     cy.getElement('user-name')
-  })
-
-  it('logging out', () => {
     cy.getElement('profile-dropdown').click()
     cy.getElement('signout-button').click()
     cy.url().should('include', '/auth/login')
