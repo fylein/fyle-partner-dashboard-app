4a6861b32c04b4e0aca23f17bed3d502ad00832e
anishfyle <anishkumar.s@fyle.in>
2022-11-14 14:24:27 +0530

    Fixes/Assert Addition

--- cypress/e2e/001-micro-actions/005-detail-view.cy.js
+++ cypress/e2e/001-micro-actions/005-detail-view.cy.js
@@ -8,11 +8,13 @@ describe('detail-view', () => {
   
   it('loads partner-dashboard app : check detail-view', () => {
     cy.wait('@OrgDetails').its('response.statusCode').should('equal', 200)
-    cy.getElement('detail-view-tab').click()
+    cy.getElement('detail-view-tab')
     cy.getElement('search-input-area').type('multi')
     cy.getElement('client-name')
     cy.getElement('active-user-text')
     cy.getElement('total-users-text')
-    
+    cy.getElement('client-card-rows')
+    cy.getElement('client-card')
+    cy.getElement('card-data')
   })
 })
