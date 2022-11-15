bc765234fcefd09e6ba1953f55251c6b92620c6d
anish <anish@Aditya.local>
2022-11-15 13:59:17 +0530

    table-view E2E Test More Asserts

--- cypress/e2e/001-micro-actions/006-table-view.cy.js
+++ cypress/e2e/001-micro-actions/006-table-view.cy.js
@@ -8,9 +8,17 @@ describe('table view', () => {
   
   it('loads partner-dashboard app : check table-view', () => {
     cy.wait('@OrgDetails').its('response.statusCode').should('equal', 200)
+    cy.getElement('table-view-tab').click()
     cy.getElement('search-input-area').type('fyle')
+    cy.getElement('data-row-head')
+    cy.getElement('row-data')
     cy.getElement('clear-search').click()
     cy.getElement('search-input-area').type('multi')
+    cy.getElement('row-data')
     cy.getElement('clear-search').click()
+    cy.getElement('search-input-area').type('invalid')
+    cy.getElement('zero-state-img')
+    cy.getElement('clear-search').click()
+    cy.getElement('row-data')
   })
 })
