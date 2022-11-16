9e4eb281389dc215c45ee71c04b279e85dc7dece
anish <anish@Aditya.local>
2022-11-16 09:05:11 +0530

    More Asserts 2

--- cypress/e2e/001-micro-actions/006-table-view.cy.js
+++ cypress/e2e/001-micro-actions/006-table-view.cy.js
@@ -20,5 +20,40 @@ describe('table view', () => {
     cy.getElement('zero-state-img')
     cy.getElement('clear-search').click()
     cy.getElement('row-data')
+    cy.assertText('client-name','Client Name').click()
+    cy.getElement('sort-icon-client-name')
+    //CheckDecreasing
+    cy.getElement('client-name').click()
+    //CheckIncreasing
+    cy.assertText('active-users-text','Active Users').click()
+    cy.getElement('sort-icon-active-users')
+    //CheckDecreasing
+    cy.getElement('active-users-text').click()
+    //CheckIncreasing
+    cy.assertText('total-users-text','Total Users').click()
+    cy.getElement('sort-icon-total-users')
+    //CheckDecreasing
+    cy.getElement('total-users-text').click()
+    //CheckIncreasing
+    cy.assertText('pending-invitation-text','Pending Invitation').click()
+    cy.getElement('sort-icon-pending-invitation')
+    //CheckDecreasing
+    cy.getElement('pending-invitation-text').click()
+    //CheckIncreasing
+    cy.assertText('incomplete-expenses-text','Incomplete Expenses').click()
+    cy.getElement('sort-icon-incomplete-expenses')
+    //CheckDecreasing
+    cy.getElement('incomplete-expenses-text').click()
+    //CheckIncreasing
+    cy.assertText('reports-to-approve-text','Reports to Approve').click()
+    cy.getElement('sort-icon-reports-to-approve')
+    //CheckDecreasing
+    cy.getElement('reports-to-approve-text').click()
+    //CheckIncreasing
+    cy.assertText('pending-reimbursements-text','Pending Reimbursements').click()
+    cy.getElement('sort-icon-pending-reimbursements')
+    //CheckDecreasing
+    cy.getElement('pending-reimbursements-text').click()
+    //CheckIncreasing
   })
 })
